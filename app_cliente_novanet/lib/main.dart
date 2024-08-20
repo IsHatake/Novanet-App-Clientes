cf5ad2a866ccb3d6b635c47fda054aaba7457d94
Isaac Ramos <isaac_gomezramos@outlook.com>
2024-08-20 10:29:11 -0600

    bueno ahora si que este sea el utlimo nms

--- app_cliente_novanet/lib/main.dart
+++ app_cliente_novanet/lib/main.dart
@@ -5,13 +5,22 @@ import 'package:flutter/services.dart';
 import 'package:app_cliente_novanet/splashscreen.dart';
 import 'package:app_cliente_novanet/utils/colornotifire.dart';
 import 'package:provider/provider.dart';
-
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  final PermissionStatus notificationPermissionStatus = await Permission.notification.status;
+
+  if (notificationPermissionStatus != PermissionStatus.granted) {
+    final PermissionStatus result = await Permission.notification.request();
+
+    if (result != PermissionStatus.granted) {
+    }
+  }
 
   final notificationService = NotificationService();
   await notificationService.init();
